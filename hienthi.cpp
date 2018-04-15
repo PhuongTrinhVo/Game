e86e579e0b999154fc419375f293e1e691e89d4f
trinhnoo <37481788+trinhnoo@users.noreply.github.com>
2018-04-16 01:44:06 +0700

    Add files via upload

--- hienthi.cpp
+++ hienthi.cpp
@@ -1,49 +1,36 @@
-void hienthi(xe,vatcan,vatcan); // ham hien thi xe va vat can
-
-void hienthi(xe xe, vatcan vc1, vatcan vc2)
+void hienthi (xe xe, vatcan vc)
 {
-	int i,j;
-	for(i=-1;i<=1;i++) // hien thi vat can
-		for(j=-1;j<=1;j++)
+	system ("cls");
+	for (int i = 0; i < dong; i++)
+	{
+		gotoxy (0,i);
+		cout << "|";
+	}
+	for (int i = 0; i < cot ;i++)
+	{
+		gotoxy(cot,i);
+		cout << "|";
+	}
+	//---------------------------hien thi xe-----------------------
+	for (int kdong = -1 ; kdong<= 1 ; kdong ++)
+		for (int kcot =-1 ;kcot <=1 ; kcot++)
+		{	int x= kcot + xe.td.x;
+			int y= kdong +xe.td.y;
+			
+			gotoxy(x,y);
+			putchar (xe.hd.a[kdong+1][kcot+1]);
+		}
+	//----------------------------hien thi vat can-------------------------
+	for (int kdong = -1 ; kdong<= 1 ; kdong ++)
+		for (int kcot =-1 ;kcot <=1 ; kcot++)
 		{
-			int c,d;
-			c=j+ vc1.td.x;
-			d=i+ vc1.td.y;
-
-			if(d>=0 && d<25)
+			int x= kcot + vc.td.x;
+			int y= kdong +vc.td.y;
+			if (y >= 0)
 			{
-				gotoxy(c,d);
-		    	putchar(vc1.hd.a[i+1][j+1]);
+			gotoxy(x,y);
+			putchar (vc.hd.a[kdong+1][kcot+1]);
 			}
 		}
-
-	for(i=-1;i<=1;i++) // hien thi vat can
-	for(j=-1;j<=1;j++)
-	{
-		int p,l;
-		p=j+ vc2.td.x;
-		l=i+ vc2.td.y;
-
-		if(l>=0 && l<25)
-		{
-			gotoxy(p,l);
-		    putchar(vc2.hd.a[i+1][j+1]);
-		}
-
-	}
-
-	for(i=-1;i<=1;i++) // hien thi xe
-	for(j=-1;j<=1;j++)
-	{
-		int c,d;
-		c=j+ xe.td.x;
-		d=i+ xe.td.y;
-		gotoxy(c,d);
-		putchar(xe.hd.a[i+1][j+1]);
-	}
-
-	gotoxy(cot+15 ,10);
-	printf("DIEM  =  %d",xe.diem);
-	gotoxy(cot+15,11);
-	printf("LEVEL  = %d ",xe.level);
 }
+
