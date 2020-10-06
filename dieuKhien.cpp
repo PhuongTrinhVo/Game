f2a7ca55c657efb1b7139c454c92f53726b32baa
Vo Phuong Trinh <37481788+PhuongTrinhVo@users.noreply.github.com>
2020-10-06 17:23:14 +0700

    Revert "add function go to x point"

--- dieuKhien.cpp
+++ dieuKhien.cpp
@@ -10,15 +10,6 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(h,c);
 }
 
-void gotoXPoint(int x, int y)
-{
-	static HANDLE h = NULL;
-	if (!h)
-		h = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD c = { x, y };
-    SetConsoleCursorPosition(h,c);
-}
-
 void phanmuc()
 {
 	char nhap;
