2bfc53633792dd3df3f8e86f8e252a38890ba6b4
phungMai234 <phungtuyet234@gmail.com>
2020-10-06 17:12:15 +0700

    add function go to x point

--- dieuKhien.cpp
+++ dieuKhien.cpp
@@ -10,6 +10,15 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(h,c);
 }
 
+void gotoXPoint(int x, int y)
+{
+	static HANDLE h = NULL;
+	if (!h)
+		h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD c = { x, y };
+    SetConsoleCursorPosition(h,c);
+}
+
 void phanmuc()
 {
 	char nhap;
